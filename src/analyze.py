557452ec4b35c6de3f2665e99df3bb7827f28f1e
Tanya Kh <tanygrec@gmail.com>
2019-04-02 07:57:47 -0700

    separating reporting

--- src/analyze.py
+++ src/analyze.py
@@ -14,16 +14,6 @@ parser.add_argument("-f", "--function", help="analysis function")
 args = parser.parse_args()
 
 tickers = TickerParser(ROOT).parse_daily(args.stock)
-res = TickerAnalyzer(tickers).analyze(args.data, args.period, args.function)
-
-print("total event count: {cnt}".format(cnt=res["count"]))
-for offset in [1, 3, 7, 14, 30]:
-    print("changes after {o} days:".format(o=offset))
-    print("  higher: avg price change: {hpp:.2f}%; with events: {he} ({hep:.2f}%)".format(
-      hpp=res[offset]["sum_hi_percent_change"] / res[offset]["hi_count"] * 100,
-      he=res[offset]["hi_count"],
-      hep=res[offset]["hi_count"] / res["count"] * 100))
-    print("  lower: avg price change: {lpp:.2f}%; with events: {le} ({lep:.2f}%)".format(
-      lpp=res[offset]["sum_lo_percent_change"] / res[offset]["lo_count"] * 100,
-      le=res[offset]["lo_count"],
-      lep=res[offset]["lo_count"] / res["count"] * 100))
+analyzer = TickerAnalyzer(tickers)
+analyzer.analyze(args.data, args.period, args.function)
+print(str(analyzer.result))
--- src/ticker/analyzers.py
+++ src/ticker/analyzers.py
@@ -1,10 +1,41 @@
 import datetime
 
-class TickerAnalyzer:
+class TickerAnalysisResult:
+    def __init__(self, tickers):
+        self.tickers = tickers
+        self.count = 0
+        self.frames = [1, 3, 7, 14, 30]
+        self.stats = {}
+        for i in self.frames:
+            self.stats[i] = TickerAnalysisStats()
 
+    def __str__(self):
+        ret = "total event count: {cnt}\n".format(cnt=self.count)
+        for offset in self.frames:
+            ret += "changes after {o} days:\n".format(o=offset)
+            ret += "  higher: avg price change: {hpp:.2f}%; with events: {he} ({hep:.2f}%)\n".format(
+                hpp=self.stats[offset].sum_hi_percent_change / self.stats[offset].hi_count * 100,
+                he=self.stats[offset].hi_count,
+                hep=self.stats[offset].hi_count / self.count * 100)
+            ret += "  lower: avg price change: {lpp:.2f}%; with events: {le} ({lep:.2f}%)\n".format(
+                lpp=self.stats[offset].sum_lo_percent_change / self.stats[offset].lo_count * 100,
+                le=self.stats[offset].lo_count,
+                lep=self.stats[offset].lo_count / self.count * 100)
+
+        return ret
+
+class TickerAnalysisStats:
+    def __init__(self):
+        self.sum_lo_percent_change = 0.0
+        self.sum_hi_percent_change = 0.0
+        self.lo_count = 0
+        self.hi_count = 0
+
+class TickerAnalyzer:
     def __init__(self, tickers):
         tickers.sort(key=lambda t: t.date)
         self.tickers = tickers
+        self.result = TickerAnalysisResult(tickers)
 
     def max(self, date, period_in_days, field):
         start_date = date - datetime.timedelta(days=period_in_days)
@@ -19,9 +50,6 @@ class TickerAnalyzer:
         return min(period_list, key=lambda ticker: getattr(ticker, field))
 
     def analyze(self, field, period, function):
-        analysis = {}
-        analysis["count"] = 0
-
         for idx, ticker in enumerate(self.tickers):
             try:
                 func_ticker = getattr(self, function)(ticker.date, int(period), field)
@@ -30,25 +58,15 @@ class TickerAnalyzer:
                 if ((function == "max" and getattr(ticker, field) > result) or
                     (function == "min" and getattr(ticker, field) < result)):
 
-                    analysis["count"] += 1
-
-                    for offset in [1, 3, 7, 14, 30]:
-                        if not offset in analysis:
-                            analysis[offset] = {}
-                            analysis[offset]["sum_lo_percent_change"] = 0.0
-                            analysis[offset]["sum_hi_percent_change"] = 0.0
-                            analysis[offset]["lo_count"] = 0
-                            analysis[offset]["hi_count"] = 0
-
+                    self.result.count += 1
+                    for offset in self.result.frames:
                         change = (getattr(self.tickers[idx+offset], field) - result)
                         if getattr(self.tickers[idx+offset], field) >= result:
-                            analysis[offset]["sum_hi_percent_change"] += change/result
-                            analysis[offset]["hi_count"] += 1
+                            self.result.stats[offset].sum_hi_percent_change += change/result
+                            self.result.stats[offset].hi_count += 1
                         else:
-                            analysis[offset]["sum_lo_percent_change"] += change/result
-                            analysis[offset]["lo_count"] += 1
+                            self.result.stats[offset].sum_lo_percent_change += change/result
+                            self.result.stats[offset].lo_count += 1
 
             except (IndexError, ValueError):
                 next
-
-        return analysis
