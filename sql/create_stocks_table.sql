9c18fd0821f25c8cdfd7806a5d2d30c59e109bc4
Sergii Romanov <sergii.romanov@protonmail.com>
2019-05-21 21:00:58 -0700

    adding sqls for stocks

---
+++ sql/create_stocks_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS fine.stocks (
+  symbol VARCHAR(6) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  sector VARCHAR(50),
+  industry VARCHAR(50),
+  location VARCHAR(50),
+  cik VARCHAR(50),
+  source VARCHAR(50),
+  daily_updated TIMESTAMP DEFAULT NULL,
+  intraday_updated TIMESTAMP DEFAULT NULL,
+  deleted TINYINT DEFAULT 0,
+  PRIMARY KEY (symbol)
+) ENGINE=INNODB;
