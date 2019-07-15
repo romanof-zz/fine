5f0b4ade82e80e3de5e1d19d90823eed0018a9af
Sergii Romanov <sergii.romanov@protonmail.com>
2019-07-15 16:37:23 +0300

    s3 read retry

--- src/storage.py
+++ src/storage.py
@@ -3,7 +3,12 @@ import os
 import sys
 import boto3
 
+from botocore.exceptions import ReadTimeoutError
+
 class S3Storage:
+    MAX_RETRY = 3
+    RETRY_WAIT = 3
+
     def __init__(self, bucket):
         self.s3 = boto3.resource('s3')
         self.bucket = bucket
@@ -13,5 +18,11 @@ class S3Storage:
         obj.put(Body=data)
 
     def get(self, key):
-        obj = self.s3.Object(self.bucket, key)
-        return obj.get()['Body'].read().decode('utf-8')
+        retry_count = 0
+        while(retry_count < self.MAX_RETRY):
+            try:
+                obj = self.s3.Object(self.bucket, key)
+                return obj.get()['Body'].read().decode('utf-8')
+            except ReadTimeoutError:
+                retry_count+=1
+                sleep(self.RETRY_WAIT * retry_count)
