3984af601d624830c688984105c00a50144c0cfe
Sergii Romanov <sergii.romanov@protonmail.com>
2019-07-22 15:51:02 +0300

    exclude wrong symbols.

--- bin/stocks_convert.py
+++ bin/stocks_convert.py
@@ -4,19 +4,24 @@ import datetime
 from stocks.models import Stock
 
 names = ["nasdaq", "nyse", "amex"]
-stock_keys = set()
-stocks = []
+stocks = {}
 
 for source in names:
     with open("{}.csv".format(source), "r") as file:
         reader = csv.reader(file, delimiter=',')
         next(reader, None)  # skip the headers
         for row in reader:
-            if row[0] not in stock_keys:
-                stock_keys.add(row[0])
-                stocks.append(Stock(row[0], row[1], row[5], row[6], source))
+            smbl = row[0].strip()
+            if smbl.find('^') != -1 or smbl.find('.') != -1:
+                continue
+            if smbl not in stocks.keys():
+                stocks[smbl] = Stock(smbl, row[1], row[5], row[6], source)
+            else:
+                stocks[smbl].source += ",{}".format(source)
 
-print(stock_keys)
+print(stocks.keys())
+print(len(stocks.keys()))
 
+stocks = list(stocks.values())
 with open("us.stocks.yml", "w+") as file:
     file.write(yaml.dump(stocks))
