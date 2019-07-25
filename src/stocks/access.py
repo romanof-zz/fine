f243fa31eb927259737963b30be97c8c0746db8e
Sergii Romanov <sergii.romanov@protonmail.com>
2019-07-25 15:28:15 +0300

    data validation before updates.

--- src/stocks/access.py
+++ src/stocks/access.py
@@ -2,6 +2,7 @@ from datetime import timedelta, datetime
 import csv
 import concurrent.futures
 from botocore.exceptions import ClientError
+import pandas as pd
 import yfinance as yf
 
 from util import DATE_FORMAT
@@ -24,17 +25,23 @@ class TickerDataAccess:
         self.symbols = {}
 
     def update(self, symbols, type, period):
-        if type == Ticker.Type.OPTIONS:
-            self.__update_options(symbols)
-        else:
-            self.__update_symbols(symbols, type, period)
+        requested = len(symbols)
+        self.logger.info(f"requested symbols {requested}")
 
-        self.__reduce_updated(symbols, type)
+        if type == Ticker.Type.OPTIONS:
+            updated_symbols = self.__update_options(symbols)
+        else:
+            updated_symbols = self.__update_symbols(symbols, type, period)
+
+        updated = len(updated_symbols)
+        self.logger.info(f"updated symbols {updated}")
+        self.__reduce_updated(updated_symbols, type, requested-updated)
 
     def __update_options(self, symbols):
         data = yf.Tickers(" ".join(symbols))
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
             [executor.submit(self.__update_single_option, symbol, getattr(data, symbol)) for symbol in symbols]
+        return symbols
 
     def __update_single_option(self, symbol, obj):
         # update dividends
@@ -52,23 +59,49 @@ class TickerDataAccess:
     def __update_symbols(self, symbols, type, period):
         df = self.__yfin_download(symbols, type, period)
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            [executor.submit(self.__write_single_symbol, symbol, type, period, df[symbol] if len(symbols) > 1 else df) for symbol in symbols]
+            futures = {executor.submit(self.__write_single_symbol, symbol, type, period, df[symbol] if len(symbols) > 1 else df): symbol for symbol in symbols}
+            for future in concurrent.futures.as_completed(futures):
+                symbol = futures[future]
+                if not future.result(): symbols.remove(symbol)
+        return symbols
 
     def __write_single_symbol(self, symbol, type, period, data):
         # process 1d data as single files.
         if type == Ticker.Type.ONE_DAY:
-            self.storage.put(self.__ticker_filename(symbol, type), data.to_csv())
-            self.logger.info(f"updated {symbol} for {type}")
+            if self.__valid_data(data):
+                self.storage.put(self.__ticker_filename(symbol, type), data.to_csv())
+                self.logger.info(f"updated {symbol} for {type}")
+                return True
+            else: return False
         else:
+            error_count = 0
+            if data.empty: return False # remove non-loaded symbols
             with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-                [executor.submit(self.__write_single_date_key, symbol, type, days, data) for days in range(0, period)]
+                futures = [executor.submit(self.__write_single_date_key, symbol, type, days, data) for days in range(0, period)]
+                for future in concurrent.futures.as_completed(futures):
+                    if not future.result(): error_count+=1
+            # consider 20% missing data valid
+            self.logger.info(f"{symbol}: errors {error_count} for period {period}")
+            return error_count / period < 0.2
 
     def __write_single_date_key(self, symbol, type, days, data):
         key = (datetime.now() - timedelta(days=days)).strftime(DATE_FORMAT)
         segment = data.loc[key:key]
-        if not segment.empty:
+        if segment.empty: return True # skip weekends
+        if self.__valid_data(segment):
             self.storage.put(self.__ticker_filename(symbol, type, key), segment.to_csv())
             self.logger.info(f"updated {symbol} for {type} and date {key}")
+            return True
+        else: return False
+
+    def __valid_data(self, df):
+        if not len(df): return False
+        if (not df.iloc[0].Low) or pd.isna(df.iloc[0].Low): return False
+        if (not df.iloc[0].High) or pd.isna(df.iloc[0].High): return False
+        if (not df.iloc[0].Open) or pd.isna(df.iloc[0].Open): return False
+        if (not df.iloc[0].Close) or pd.isna(df.iloc[0].Close): return False
+        if (not df.iloc[0].Volume) or pd.isna(df.iloc[0].Volume): return False
+        return True
 
     def load(self, symbol, type, date=None):
         filename = self.__ticker_filename(self, symbol, type, date)
@@ -86,14 +119,15 @@ class TickerDataAccess:
             sdata = ",".join(US_SYMBOLS)
             self.storage.put(file, sdata)
 
-        self.symbols[type] = sdata.split(",") if sdata else []
+        self.symbols[type] = sdata.strip().split(",") if sdata else []
         self.logger.info(f"{type} total: {len(self.symbols[type])}")
 
         return self.symbols[type][:limit]
 
-    def __reduce_updated(self, symbols, type):
+    def __reduce_updated(self, symbols, type, offset):
         if not type in self.symbols: return
         self.symbols[type] = [s for s in self.symbols[type] if s not in symbols]
+        for i in range(0, offset): self.symbols[type].append(self.symbols[type].pop(0))
         sdata = ",".join(self.symbols[type]) if self.symbols[type] else ''
         self.storage.put(self.__update_filename(type), sdata)
 
