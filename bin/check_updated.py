791b31314b50e355f2debd97d8f7b0fd60c7480b
Sergii Romanov <sergii.romanov@protonmail.com>
2019-07-25 12:05:22 +0300

    adding concurency into check updates script.

--- bin/check_updated.py
+++ bin/check_updated.py
@@ -1,16 +1,29 @@
 from app import AppContext
+import concurrent.futures
 
 from stocks.symbols import US_SYMBOLS
 from botocore.errorfactory import ClientError
 
 app = AppContext()
 
-missing = []
-for symbol in US_SYMBOLS:
+def check_not_exists(symbol):
     try:
-        app.s3.get(f"tickers/{symbol}/1h/2019-07-24.csv")
+        type = '5m'
+        date = '2019-07-22'
+        content = app.s3.get(f"tickers/{symbol}/{type}/{date}.csv")
+        return None if date in content else symbol
     except ClientError:
-        missing.append(symbol)
+        return symbol
+
+missing = []
+counter = 0
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    futures = [executor.submit(check_not_exists, symbol) for symbol in US_SYMBOLS]
+    for future in concurrent.futures.as_completed(futures):
+        counter+=1
+        print(counter)
+        res = future.result()
+        if res: missing.append(res)
 
 print(missing)
 print(len(missing))
